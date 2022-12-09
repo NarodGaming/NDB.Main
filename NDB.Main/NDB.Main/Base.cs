f9860538a6530401478b499f09c122d4b6d6749d
NarodGaming <arron.faulkner@gmail.com>
2022-12-09 00:45:33 +0000

    Fixed bug with hardcoded prefix
    Change prefix to rely on configuration setting rather than pre-determined string prefix.

--- NDB.Main/NDB.Main/Base.cs
+++ NDB.Main/NDB.Main/Base.cs
@@ -95,7 +95,7 @@ public static class NDB_Main
 
         int pos = 0;
 
-        if (userMessage.HasStringPrefix("+", ref pos) || userMessage.HasMentionPrefix(_client.CurrentUser, ref pos))
+        if (userMessage.HasStringPrefix(_config["stringPrefix"], ref pos) || userMessage.HasMentionPrefix(_client.CurrentUser, ref pos))
         {
             IResult result = await _commands.ExecuteAsync(context, pos, _services);
         }
